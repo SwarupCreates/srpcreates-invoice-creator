1af9700d6b212f1aac955fe32148c5c891e71629
Swarup R. Paul <121659337+SwarupCreates@users.noreply.github.com>
2026-07-03 22:42:53 +0530

    Initialize TypeScript React app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Hello World</h1>
+      <p>This is a TypeScript React app.</p>
+    </main>
+  )
+}
+
+export default App
